d2c3550cf5a860893f56285c5dae03a3f2c78b6f
Simen Eilevstjønn <simene@lyse.net>
2020-02-07 15:37:11 +0100

    Week conversion logic

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -18,4 +18,42 @@ document.getElementById("giga").innerHTML = new Date().getWeek();
 
 //Attach onclick events to navbar items
 document.getElementById("current").addEventListener("click", function() {document.getElementById("thiswk").style.display = "block";document.getElementById("conv").style.display = "none";document.getElementById("current").className = "nav-link active";document.getElementById("convert").className = "nav-link";});
-document.getElementById("convert").addEventListener("click", function() {document.getElementById("thiswk").style.display = "none";document.getElementById("conv").style.display = "block";document.getElementById("current").className = "nav-link";document.getElementById("convert").className = "nav-link active";});
+document.getElementById("convert").addEventListener("click", function() {
+    document.getElementById("thiswk").style.display = "none";
+    document.getElementById("conv").style.display = "block";
+    document.getElementById("current").className = "nav-link";
+    document.getElementById("convert").className = "nav-link active";
+    document.getElementById("weeknum").value = new Date().getWeek();
+    runConvert();
+    document.getElementById("weeknum").addEventListener("change", runConvert);
+});
+
+function weekStartDate(weeknum) {
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    //Generate an object for week 1
+    var week1 = new Date(date.getFullYear(), 0, 4);
+
+    //Find first day of week 1
+    var j4d = week1.getDay();
+    if (j4d == 0) j4d = 7;
+    week1 = new Date(week1.getTime() - (j4d - 1) * 24 * 60 * 60 * 1000);
+
+    //Add weeks and return
+    return new Date(week1.getTime() + 7 * 24 * 60 * 60 * 1000 * (weeknum  - 1));
+}
+
+function runConvert() {
+    //Grab input and run conversion
+    var selweek = document.getElementById("weeknum").value;
+    if (selweek > 53) {
+        selweek = 53;
+        document.getElementById("weeknum").value = 53;
+    }
+    var weekx = weekStartDate(selweek);
+
+    //Print results
+    document.getElementById("startdate").innerHTML = "Monday " + weekx.toLocaleDateString();
+    document.getElementById("enddate").innerHTML = "Sunday " + new Date(weekx.getTime() + 6 * 24 * 60 * 60 * 1000).toLocaleDateString();
+}
